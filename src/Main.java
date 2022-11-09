f3d350213ca08b9becb4043af3502cb3b4a1adca
Vytis Lasaitis <v.lasaitis@gmail.com>
2022-11-09 11:48:42 +0100

    new feature

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,9 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("First change");
         System.out.println("Compromise");
+        newFeature();
+    }
+    public static void newFeature(){
+        System.out.println("New");
     }
 }
